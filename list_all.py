432e3ec7b151f89a9d97a98509ce63bf15547991
Carlos Sanchiz <carlos@amazon.com>
2016-06-25 10:15:00 +0200

    Adding EBS and Snapshots scripts Also adding regions file

--- list_all.py
+++ list_all.py
@@ -4,7 +4,10 @@
 
 import boto3
 
-REGIONS=["us-east-1", "us-west-1", "us-west-2", "eu-west-1", "eu-central-1", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "sa-east-1"]
+#REGIONS=["us-east-1", "us-west-1", "us-west-2", "eu-west-1", "eu-central-1", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "sa-east-1"]
+
+file = open('regions', 'r')
+REGIONS=file.read().splitlines()
 
 for region in REGIONS:
 
